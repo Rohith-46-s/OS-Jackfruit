32ddf1458029691e8f82e7edfbae50966b6075b2
Rohith-46-s <rohithchintu29@gmail.com>
2026-04-16 23:55:14 +0530

    Add monitor_entry_info and monitor_snapshot structures

--- boilerplate/monitor_ioctl.h
+++ boilerplate/monitor_ioctl.h
@@ -10,6 +10,7 @@
 #endif
 
 #define MONITOR_NAME_LEN 32
+#define MONITOR_MAX_SNAPSHOT 64
 
 struct monitor_request {
     pid_t pid;
@@ -18,8 +19,22 @@ struct monitor_request {
     char container_id[MONITOR_NAME_LEN];
 };
 
+struct monitor_entry_info {
+    pid_t pid;
+    unsigned long soft_limit_bytes;
+    unsigned long hard_limit_bytes;
+    int soft_limit_exceeded;
+    char container_id[MONITOR_NAME_LEN];
+};
+
+struct monitor_snapshot {
+    unsigned int count;
+    struct monitor_entry_info entries[MONITOR_MAX_SNAPSHOT];
+};
+
 #define MONITOR_MAGIC 'M'
 #define MONITOR_REGISTER _IOW(MONITOR_MAGIC, 1, struct monitor_request)
 #define MONITOR_UNREGISTER _IOW(MONITOR_MAGIC, 2, struct monitor_request)
+#define MONITOR_LIST _IOR(MONITOR_MAGIC, 3, struct monitor_snapshot)
 
 #endif
